ac7855bacbaffe58fffe3bf2b6e3088b6d82fa15
Peter Moroz <ptr.moroz@gmail.com>
2021-03-10 21:22:53 +0300

    add: support of SD card, write info into log file

--- gps_tracker.ino
+++ gps_tracker.ino
@@ -1,5 +1,6 @@
 /*
  * https://www.elithecomputerguy.com/2020/06/arduino-gps-adafruit-ultimate-gps-coordinates-and-speed-display-on-lcd/
+ * https://www.elithecomputerguy.com/2020/06/arduino-gps-gps-tracker-to-sd-card-adafruit-ultimate-gps/
  */
 
 #include <Adafruit_GPS.h>
@@ -11,6 +12,10 @@
 
 LiquidCrystal_I2C lcd(0x27,16,2);
 
+#include <SPI.h>
+#include <SD.h>
+
+File logFile;
 
 // Connect the GPS Power pin to 5V
 // Connect the GPS Ground pin to ground
@@ -25,21 +30,31 @@ Adafruit_GPS gps(&swSerial);
 // Set to 'true' if you want to debug and listen to the raw GPS sentences
 #define GPSECHO  true
 
+#define SD_CS_PIN 4
+
 
 void setup() {
-  // put your setup code here, to run once:
   lcd.init();
   lcd.backlight();
   lcd.setCursor(0, 0);  // col, row
-  // lcd.print("Ywrobot Arduino!");
-  lcd.setCursor(0, 1);
-  // lcd.print("Arduino LCM IIC");
+
+  lcd.print(F("Wait SD"));
+
+  // Checks for SD Card, if not found keeps loading.
+  if (!SD.begin(SD_CS_PIN)) {
+    lcd.setCursor(0, 1);
+    lcd.print(F("No SD Card!"));
+    delay(2000);
+  } 
+
+  lcd.setCursor(0, 0);  // col, row
+  lcd.print(F("SD Ready"));
 
   // connect at 115200 so we can read the GPS fast enough and echo without dropping chars
   // also spit it out
   Serial.begin(115200);
   delay(5000);
-  Serial.println("Adafruit GPS library basic test!");
+
   
   // 9600 NMEA is the default baud rate for Adafruit MTK GPS's- some use 4800
   gps.begin(9600);  
@@ -62,13 +77,14 @@ void setup() {
   delay(1000);
   // Ask for firmware version
   swSerial.println(PMTK_Q_RELEASE);
+
+  lcd.setCursor(0, 0);  // col, row
+  lcd.print(F("Setup complete"));  
   
 }
 
 uint32_t timer = millis();
 void loop() {
-  // put your main code here, to run repeatedly:
-
   char c = gps.read();
   // if you want to debug, this is a good time to do it!
   if ((c) && (GPSECHO))
@@ -90,7 +106,7 @@ void loop() {
   if (millis() - timer > 2000) {
     timer = millis(); // reset the timer
 
-    Serial.print("\nTime: ");
+    Serial.print(F("\nTime: "));
     if (gps.hour < 10) { Serial.print('0'); }
     Serial.print(gps.hour, DEC); Serial.print(':');
     if (gps.minute < 10) { Serial.print('0'); }
@@ -98,41 +114,41 @@ void loop() {
     if (gps.seconds < 10) { Serial.print('0'); }
     Serial.print(gps.seconds, DEC); Serial.print('.');
     if (gps.milliseconds < 10) {
-      Serial.print("00");
+      Serial.print(F("00"));
     } else if (gps.milliseconds > 9 && gps.milliseconds < 100) {
-      Serial.print("0");
+      Serial.print(F("0"));
     }
     Serial.println(gps.milliseconds);
-    Serial.print("Date: ");
+    Serial.print(F("Date: "));
     Serial.print(gps.day, DEC); Serial.print('/');
-    Serial.print(gps.month, DEC); Serial.print("/20");
+    Serial.print(gps.month, DEC); Serial.print(F("/20"));
     Serial.println(gps.year, DEC);
-    Serial.print("Fix: "); Serial.print((int)gps.fix);
-    Serial.print(" quality: "); Serial.println((int)gps.fixquality);
+    Serial.print(F("Fix: ")); Serial.print((int)gps.fix);
+    Serial.print(F(" quality: ")); Serial.println((int)gps.fixquality);
     if (gps.fix) {
-      Serial.print("Location: ");
+      Serial.print(F("Location: "));
       Serial.print(gps.latitude, 8); Serial.print(gps.lat);
-      Serial.print(", ");
+      Serial.print(F(", "));
       Serial.print(gps.longitude, 8); Serial.println(gps.lon);
-      //ETCG Notes -- The Following Gives Coordinates in formant you can plug into
+      //The Following Gives Coordinates in formant you can plug into
       //Google Maps (Degrees)
-      Serial.println("Location in Degrees");
+      Serial.println(F("Location in Degrees"));
       Serial.print(gps.latitudeDegrees, 8);
-      Serial.print(", ");
+      Serial.print(F(", "));
       Serial.println(gps.longitudeDegrees, 8);
 
-      Serial.print("Speed (knots): "); Serial.println(gps.speed);
-      Serial.print("Angle: "); Serial.println(gps.angle);
-      Serial.print("Altitude: "); Serial.println(gps.altitude);
-      Serial.print("Satellites: "); Serial.println((int)gps.satellites);
+      Serial.print(F("Speed (knots): ")); Serial.println(gps.speed);
+      Serial.print(F("Angle: ")); Serial.println(gps.angle);
+      Serial.print(F("Altitude: ")); Serial.println(gps.altitude);
+      Serial.print(F("Satellites: ")); Serial.println((int)gps.satellites);
 
-//        //ETCG Notes -- Print to LCD Screen
+//        //Print to LCD Screen
 //        lcd.clear();
 //        lcd.setCursor(0,1);
 //        lcd.print("Spd: ");
 //        lcd.setCursor(5,1);
-//        float mph;
-//        mph = (gps.speed) * 1.15;
+      float mph;
+      mph = (gps.speed) * 1.15;
 //        lcd.print(mph);
 //        lcd.setCursor(0,2);
 //        lcd.print("Lat: ");
@@ -142,6 +158,32 @@ void loop() {
 //        lcd.print("Lon: ");
 //        lcd.setCursor(5,3);
 //        lcd.print(gps.longitudeDegrees, 8);
+
+      //Write to SD Card
+      logFile = SD.open(F("gpsLog.txt"), FILE_WRITE);
+ 
+      String timeStamp = String(gps.day, DEC) + (",") + String(gps.month, DEC) + (",") + String(gps.year, DEC) + (" - ")
+                         + String(gps.hour, DEC) + (":") + String(gps.minute, DEC) + (":") + String(gps.seconds, DEC);
+
+      if (logFile) {
+        logFile.print(F("Time Stamp: "));
+        logFile.print(timeStamp);
+        logFile.print(F(" Speed: "));
+        logFile.print(mph);
+        logFile.print(F(" Lat/Lon: "));
+        logFile.print(gps.latitudeDegrees, 8);
+        logFile.print(F(" / "));
+        logFile.println(gps.longitudeDegrees, 8);
+        logFile.close();
+
+        lcd.setCursor(0, 0);
+        lcd.print(F("gpsLog.txt OK"));
+      } else {
+        lcd.setCursor(0, 0);
+        lcd.print(F("gpsLog.txt ERR"));
+      }
+
+
     } else {
 //        lcd.clear();
 //        lcd.setCursor(0,1);
